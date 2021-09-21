1a5c38ff419f43bb1e5267dc6b2373241fa789f9
Gourav Mishra <88934396+gouravmarch20@users.noreply.github.com>
2021-09-21 20:18:48 +0530

    update

--- app.js
+++ app.js
@@ -1,64 +1,90 @@
 var readlineSync = require('readline-sync')
 const chalk = require('chalk')
-var userName = readlineSync.question('May I have your name? ')
-
-console.log(chalk.yellowBright.bgMagenta`Hello ${userName}, Enter a / b / c to answer the questions. For each right answer, you'll get 1 point.`
-)
+console.log('welcome to pokemon quiz')
 let score = 0
-const questionAndAnswer = [
-  {
-    question: 'the language i coded most : \n A. js \n B.cpp \n C.python',
-    answer: 'A'
-  },
+const levelOneData = [
   {
     question:
-      'my currently favorate book : \n A. think & grow rich \n B.Deep work \n C. richest man in babylon',
-    answer: 'A'
-  },
-  {
-    question: 'city where i live currently \nA.Tokyo \nB.Delhi \nC.Ranchi',
-    answer: 'B'
-  },
-  {
-    question: 'my favorate stock \nA.Affle \nB.Info Edge  \nC.ITC',
+      'what is full name ash : \n A. Ash Red \n B. Ash okhle \n C. Ash ketchum',
     answer: 'C'
   },
   {
     question:
-      'City which i wanted to visit  \nA.Los Angeles \nB.Mumbai \nC.Paris',
+      'What Pokémon did Ash start with  : \n A. Pikachu \n B. zeron  \n C. Squirtle ',
+    answer: 'A'
+  },
+  {
+    question:
+      'which city ash ketchum live  \nA.Tokyo \nB.Pallet Town \nC.Kanto ',
+    answer: 'B'
+  },
+  {
+    question:
+      'first pokemon ash catch  \nA.Bulbasaur \nB.Caterpie \nC.Pidgeotto ',
+    answer: 'B'
+  },
+  {
+    question: 'Which type pokemon charizard is   \nA.Water \nB.Fire \nC.Rock',
+    answer: 'B'
+  },
+  {
+    question:
+      'squirtle evolve inot   \nA.Wartortle  \nB.charmander  \nC.Venusaur',
+    answer: 'A'
+  }
+]
+const levelSecondData = [
+  {
+    question:
+      'ash wanted to be  : \n A. Pokemon master \n B. React native developer \n C. Pokemon trainer',
+    answer: 'A'
+  },
+  {
+    question:
+      'Pokémon franchise owned by : \n A. Walt Diseny \n B. Warner Brother  \n C. Nintendo',
+    answer: 'C'
+  },
+  {
+    question:
+      'Brock wanted to be   : \n A. Gym Trainer  \n B. Gym Leader  \n C. Pokemon Trainer',
     answer: 'B'
   }
 ]
-function userScore (question, answer, index) {
+function startGame (question, answer, index) {
   let userInput = readlineSync.question(
     index + 1 + '.' + question + '\n ' + `Answer ${index + 1} >> `
   )
   if (userInput.toUpperCase() == answer) {
     score = ++score
     console.log(
-      chalk.blackBright.bgGreen.bold`congrats ${chalk.yellowBright(
-        userName
-      )}, 🤳🎉 your score ${chalk.yellowBright(score)} `
+      chalk.blackBright.bgGreen
+        .bold`congrats ${chalk.yellowBright()}, 🤳🎉 your score ${chalk.yellowBright(
+        score
+      )} `
     )
   } else {
-    console.log(
-      chalk.blackBright.bgRed
-        .bold` 😱 😤 wrong answer  & your score ${chalk.green(score)} 😓`
-    )
+    console.log(chalk.blackBright.bgRed` wrong answer 😨 ❗❗`)
   }
 }
 
-for (let index = 0; index < questionAndAnswer.length; index++) {
-  userScore(
-    questionAndAnswer[index].question,
-    questionAndAnswer[index].answer,
-    index
-  )
-
-  console.log(score)
+for (let index = 0; index < levelOneData.length; index++) {
+  startGame(levelOneData[index].question, levelOneData[index].answer, index)
 }
-console.log(chalk.blackBright.bgBlue.bold`---Leaderboard---`)
-
-console.log(`Prashant : 5`)
-console.log(`Somu : 5`)
-console.log(`Abhishek : 5`)
+if (score > 4) {
+  console.log(chalk.blackBright.bgBlueBright` You entered to level 2 `)
+  for (let index = 0; index < levelSecondData.length; index++) {
+    startGame(
+      levelSecondData[index].question,
+      levelSecondData[index].answer,
+      index
+    )
+    console.log(
+      chalk.blackBright.bgBlueBright`You finished the game total score ` + score
+    )
+  }
+} else {
+  console.log(
+    chalk.blackBright.bgRedBright`you failed to enter level 2  total score ` +
+      score
+  )
+}
